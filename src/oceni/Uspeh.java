6d0ca9f2a6cda0ac1aaab69b08180a69119990fe
david.elim45 <davidelim170204@gmail.com>
2024-01-23 18:00:45 +0100

    Student

---
+++ src/oceni/Uspeh.java
@@ -0,0 +1,23 @@
+package oceni;
+
+public class Uspeh {
+
+	public static void metodUspeh(double bodovi) {
+		if (bodovi >=8,5) {
+			System.out.println ("odlicen");
+			}else if (bodovi >=7,5) {
+				System.out.println("mn dobar");
+			}else if (bodovi >=5,5) {
+				System.out.println("mn dobar");
+			}else if (bodovi >=3,5) {
+				System.out.println("mn dobar");
+			}else   {
+				System.out.println("nedovolen");
+				}
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
